1edf978ffd893babf57134e291b14ddca1365f26
Piumal Dhanawardhana <24piumal@gmail.com>
2023-03-31 15:12:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function GitHubUser({name, location, avatar}){
+  return(
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name}/>
+    </div>
+    );
+}
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/piumonline`
+    )
+    .then((response)=>response.json())
+    .then(setData);
+  },[])
+
+  // if(data) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GitHubUser 
+      name={data.name} 
+      location={data.location}
+      avatar={data.avatar_url}/>
+  )
 }
 
 export default App;
